a7cf1e62817e4936e658828ddeb5f35b72dcfa86
AYAN-AMBESH <ambesh12k@gmail.com>
2024-10-22 20:46:09 +0530

    Updated Code but unable to print result of scanner

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Rusty_Saber
+
 an migration of SaberAV in rust
+
+# For Ubuntu/Debian install the bellow package
+
+```bash
+sudo apt-get install libyara-dev
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,57 @@
-slint::include_modules!();
-fn main() {
-    let sabui = Saber::new().expect("unkown error").run();
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::{fs, io};
+use yara_x;
+use yara_x::{ScanError, ScanResults};
 
+struct Scandir {
+    dir_to_scan: &'static str,
+    yara_rule_path: &'static str,
+}
+
+impl Scandir {
+    fn new(folder_to_scan: &'static str, rule_for_scan: &'static str) -> Scandir {
+        Scandir {
+            dir_to_scan: folder_to_scan,
+            yara_rule_path: rule_for_scan,
+        }
+    }
+
+    fn scanning(&self) -> io::Result<()> {
+        let mut compiler = yara_x::Compiler::new();
+        let mut sourceofyara = OpenOptions::new()
+            .read(true)
+            .open(self.yara_rule_path)
+            .expect("Unable to open yara rule");
+
+        let mut buffer = Vec::new();
+        let _ = sourceofyara.read_to_end(&mut buffer);
+
+        let yara_content: &[u8] = &buffer;
+
+        compiler.add_source(yara_content).unwrap();
+
+        let rules = compiler.build();
+
+        let entries = fs::read_dir(self.dir_to_scan)?
+            .map(|res| res.map(|e| e.path()))
+            .collect::<Result<Vec<_>, io::Error>>()?;
+
+        for files in &entries {
+            // println!("Path for file: {}", files.display());
+            let mut scanner = yara_x::Scanner::new(&rules);
+            let scan_results: Result<ScanResults, ScanError> = scanner.scan_file(files);
+        }
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let test = Scandir::new(
+        "/home/ambesh/personal/rustySaber/scantestdir/",
+        "/home/ambesh/personal/rustySaber/test.yar",
+    );
+
+    let _ = test.scanning();
 }
